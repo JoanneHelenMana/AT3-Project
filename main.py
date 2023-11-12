e3ea09fae031d0e1e64c3d86bf3e62d9beb85b62
JoanneHelenMana <joannehmana@gmail.com>
2023-11-12 19:04:31 +0800

    created classes

---
+++ main.py
@@ -0,0 +1,10 @@
+from player import Player
+
+
+def play():
+    player = Player()
+    print(player.backpack)
+
+
+if __name__ == "__main__":
+    play()
---
+++ player.py
@@ -0,0 +1,23 @@
+from backpack import Backpack
+
+
+class Player:
+
+    def __init__(self, name=None, backpack=None):
+        if name is None:
+            self.name = self.set_name()
+
+        if backpack is None:
+            self.backpack = self.create_backpack()
+
+    @staticmethod
+    def set_name():
+        """Sets the player's name."""
+        name = input(f"What's your name, player?\n")
+        return name
+
+    @staticmethod
+    def create_backpack():
+        """Creates the player's backpack."""
+        backpack = Backpack()
+        return backpack
